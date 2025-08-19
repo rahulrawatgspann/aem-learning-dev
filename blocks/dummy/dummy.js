a181d744f9542dd5c7ac29894eecbfeffadaf301
rahulrawatgspann <rahul.rawat@gspann.com>
2025-08-19 12:52:51 +0530

    minor2

--- blocks/dummy/dummy.js
+++ blocks/dummy/dummy.js
@@ -1,7 +1,7 @@
 export default function decorate(block) {
   const [quoteWrapper] = block.children;
 
-  const blockdummy = document.createElement('blockdummy');
+  const blockdummy = document.createElement('blockquote');
   blockdummy.textContent = quoteWrapper.textContent.trim();
   quoteWrapper.replaceChildren(blockdummy);
 }
