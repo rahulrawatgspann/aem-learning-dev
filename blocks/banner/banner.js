8926868741f7d9a1e3f6dad1a1384a7cd83cc0e0
namankotnal1 <naman.kotnala@gspann.com>
2025-08-27 16:11:20 +0530

    banner initial

---
+++ blocks/banner/banner.js
@@ -0,0 +1,25 @@
+export default function decorate(block) {
+  const picture = block.querySelector('image');
+  
+    picture.setAttribute('loading', 'lazy');
+  
+  console.log('images.......................................', picture);
+  // const img = pictureTag?.querySelector('img');
+
+  // if (img) {
+  //   img.setAttribute('loading', 'eager');
+  //   pictureTag.classList.add('hero-image');
+  // }
+
+  // block.classList.add('hero-banner');
+
+  // const headingTag = block.querySelector('h1');
+  // if (headingTag) {
+  //   headingTag.classList.add('hero-title');
+
+  //   const wrapper = document.createElement('div');
+  //   wrapper.className = 'hero-text-wrapper';
+  //   wrapper.appendChild(headingTag);
+  //   block.appendChild(wrapper);
+  // }
+}
